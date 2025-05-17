dbc59435a3664d03ce647c3476e12f42ea6471de
KmNeetuSingh <itisneetusingh@gmail.com>
2025-05-17 20:17:18 +0530

    Day-10

--- Day-10/@latest/src/main.jsx
+++ Day-10/@latest/src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { ThemeProvider } from "./App"
 
 createRoot(document.getElementById('root')).render(
-  <ThemeProvider>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </ThemeProvider>
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
