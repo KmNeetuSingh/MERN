2782198fb239e033484b8c067fce6346e5a884f1
KmNeetuSingh <itisneetusingh@gmail.com>
2025-06-04 22:02:10 +0530

    Day-14

--- Day-14/index.js
+++ Day-14/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const Port = 3000;
-// console.log(Port)
+console.log(Port)
 app.get("/health" , (req, res) => {
     res.end("Server health is good")
 })
