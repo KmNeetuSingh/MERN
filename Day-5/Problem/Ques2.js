477b6ee9f965a79d8ae33cffeb6aab1af50c4129
KmNeetuSingh <itisneetusingh@gmail.com>
2025-06-09 20:03:59 +0530

    flattened Array completed

--- Day-5/Problem/Ques2.js
+++ Day-5/Problem/Ques2.js
@@ -1,15 +1,30 @@
-function falttedArr(arr){
+// function falttedArr(arr){
+//     let res = []
+//     for(let i = 0 ; i < arr.length ; i++){
+//         if (Array.isArray(arr[i])){
+//             for (let j = 0 ; j <arr[i].length ; j++){
+//                  res.push(arr[i][j])
+//             }
+//         }
+//         else {
+//             res.push(arr[i])
+//         }
+//     }
+//     return res
+// }
+// console.log(falttedArr([1,2,3,[3,[3,4]], [4,5]]))
+function flattenedArray(arr){
     let res = []
-    for(let i = 0 ; i < arr.length ; i++){
-        if (Array.isArray(arr[i])){
-            for (let j = 0 ; j <arr[i].length ; j++){
-                 res.push(arr[i][j])
+    for(let i = 0 ; i <arr.length; i++){
+        if(Array.isArray(arr[i])){
+            for(let j = 0 ; j <arr[i].length ; j++){
+               res.push(arr[i][j])
             }
         }
-        else {
+        else{
             res.push(arr[i])
         }
     }
     return res
 }
-console.log(falttedArr([1,2,3,[3,[3,4]], [4,5]]))
+console.log(flattenedArray([1,3 ,[2,3]]))
---
+++ Day-5/Problem/as.js
@@ -0,0 +1,15 @@
+function flattened(arr){
+    let res = []
+    for(let i = 0 ; i <arr.length ; i++){
+        if(Array.isArray(arr[i])){
+            for(let j = 0 ; j <arr[i].length ; j++){
+                res.push(arr[i][j])
+            }
+        }
+        else{
+            res.push(arr[i])
+        }
+    }
+    return res
+}
+console.log(flattened([1,2,3,[3,3]]))
