fc7a3a29ab7ea278fadbf46a9390d2c6d435d56f
KmNeetuSingh <itisneetusingh@gmail.com>
2025-06-21 11:04:59 +0530

    added

--- Day-5/Problem/Add.js
+++ Day-5/Problem/Add.js
@@ -1,16 +1,15 @@
-function flattedArr(arr){
+function flattedArray(arr){
     let res = []
-    for(let i = 0 ; i<arr.length ; i++){
-        if(Array.isArray(arr[i])){
-            for(let j = 0 ; j < arr[i].length ; j++){
+    for(let i = 0 ; i <arr.length ; i++){
+        if (Array.isArray(arr[i])){
+            for(let j = 0 ; j <arr[i].length ; j++){
                 res.push(arr[i][j])
             }
         }
         else {
             res.push(arr[i])
         }
- }
- return res
- 
+    }
+    return res
 }
-console.log(flattedArr([1,2,3,[4,5]]))
+console.log(flattedArray([1,2,2,3,[4,5]]))
--- Day-5/Problem/duplaicte.js
+++ Day-5/Problem/duplaicte.js
@@ -1,19 +1,17 @@
-function duplicate(arr){
-  let res = []
-  for(let i = 0 ; i<arr.length ; i++){
-    let duplicate = false
-    for(let j = 0 ; j<res.length ; j++){
-      if(arr[i] === res[j]){
-        duplicate = true
-        break
+function duplicate(arr) {
+  let res = [];
+  for (let i = 0; i < arr.length; i++) {
+    let duplicate = false;
+    for (let j = 0; j < res.length; j++) {
+      if (arr[i] === res[j]) {
+        duplicate = true;
+        break;
       }
-
     }
-    if (!duplicate){
+    if (!duplicate) {
       res.push(arr[i])
     }
-  }
-  return res
-
+  }return res
 }
-console.log(duplicate([1,2,321,11,1,2]))
+
+console.log(duplicate([1,2,3,1,1,3]))
