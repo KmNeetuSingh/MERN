47344247b54eb5bc93a1c35e44d3984edf26fd86
KmNeetuSingh <itisneetusingh@gmail.com>
2025-06-23 10:47:09 +0530

    added

--- Day-5/Problem/secondmax.js
+++ Day-5/Problem/secondmax.js
@@ -1,16 +1,16 @@
 function secondmax(arr){
     let max = -Infinity
     let secondmax = -Infinity
-    for(let i = 0 ; i <arr.length ; i++){
+    for(let i = 0 ; i < arr.length ; i++){
         let num = arr[i]
-        if(num > max){
+        if(num >max){
             secondmax = max
             max = num
         }
-        else if (num >secondmax && num < max){
-            secondmax = num
+        else {
+            secondmax = max
         }
     }
-    return max === -Infinity ? null : max
+    return  secondmax === -Infinity ? null : secondmax
 }
-console.log(secondmax([1,2,3,31]))
+console.log(secondmax([1,2,3,3,4]))
